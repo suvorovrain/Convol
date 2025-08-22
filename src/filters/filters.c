9ba661a71e06b773d0fee8ce11c9d678653ca702
Rodion Suvorov <rodion.suvorov.94@mail.ru>
2025-08-22 07:39:18 +0300

    feat: change filter structure

--- src/filters/filters.c
+++ src/filters/filters.c
@@ -1,21 +1,360 @@
 #include "filters.h"
 
-const tiny_convolution_filter tiny_blur_filter = {
-    .matrix = {
-        {0.0, 1.0, 0.0},
-        {1.0, 1.0, 1.0},
-        {0.0, 1.0, 0.0},
-    },
-    .factor = 1.0 / 5.0,
+//----------------------------------------------------------------------------------------------
+// Blur
+//----------------------------------------------------------------------------------------------
+
+const small_convolution_filter small_blur_filter = {.matrix =
+                                                        {
+                                                            {0.0, 0.0, 1.0, 0.0, 0.0},
+                                                            {0.0, 1.0, 1.0, 1.0, 0.0},
+                                                            {1.0, 1.0, 1.0, 1.0, 1.0},
+                                                            {0.0, 1.0, 1.0, 1.0, 0.0},
+                                                            {0.0, 0.0, 1.0, 0.0, 0.0},
+                                                        },
+                                                    .factor = 1.0 / 13.0,
+                                                    .bias = 0.0};
+const medium_convolution_filter medium_blur_filter = {.matrix =
+                                                          {
+                                                              {0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0},
+                                                              {0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0},
+                                                              {0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0},
+                                                              {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0},
+                                                              {0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0},
+                                                              {0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0},
+                                                              {0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0},
+
+                                                          },
+                                                      .factor = 1.0 / 25.0,
+                                                      .bias = 0.0};
+const big_convolution_filter big_blur_filter = {
+    .matrix =
+        {
+            {0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0},
+            {0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0},
+            {0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0},
+            {0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0},
+            {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0},
+            {0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0},
+            {0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0},
+            {0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0},
+            {0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0},
+        },
+    .factor = 1.0 / 41.0,
     .bias = 0.0};
 
-const small_convolution_filter small_blur_filter = {
-    .matrix = {
-        {0.0, 0.0, 1.0, 0.0, 0.0},
-        {0.0, 1.0, 1.0, 1.0, 0.0},
-        {1.0, 1.0, 1.0, 1.0, 1.0},
-        {0.0, 1.0, 1.0, 1.0, 0.0},
-        {0.0, 0.0, 1.0, 0.0, 0.0},
-    },
-    .factor = 1.0 / 5.0,
+convolution_filter *create_blur_convolution_filter(enum strength str)
+{
+    switch (str)
+    {
+    case SMALL:
+        convolution_filter *filter = malloc(sizeof(convolution_filter));
+        if (!filter)
+        {
+            error("ERROR: malloc failed\n");
+            return NULL;
+        }
+        filter->matrix = small_blur_filter.matrix;
+        filter->bias = small_blur_filter.bias;
+        filter->factor = small_blur_filter.factor;
+        filter->size = SMALL_FILTER_SIZE;
+        return filter;
+    case MEDIUM:
+        convolution_filter *filter = malloc(sizeof(convolution_filter));
+        if (!filter)
+        {
+            error("ERROR: malloc failed\n");
+            return NULL;
+        }
+        filter->matrix = medium_blur_filter.matrix;
+        filter->bias = medium_blur_filter.bias;
+        filter->factor = medium_blur_filter.factor;
+        filter->size = MEDIUM_FILTER_SIZE;
+        return filter;
+    case BIG:
+        convolution_filter *filter = malloc(sizeof(convolution_filter));
+        if (!filter)
+        {
+            error("ERROR: malloc failed\n");
+            return NULL;
+        }
+        filter->matrix = big_blur_filter.matrix;
+        filter->bias = big_blur_filter.bias;
+        filter->factor = big_blur_filter.factor;
+        filter->size = BIG_FILTER_SIZE;
+        return filter;
+    default:
+        error("ERROR: invalid filter size");
+        return NULL;
+    }
+}
+
+//----------------------------------------------------------------------------------------------
+// Motion blur
+//----------------------------------------------------------------------------------------------
+
+const small_convolution_filter small_motion_blur_filter = {.matrix =
+                                                               {
+                                                                   {1.0, 0.0, 0.0, 0.0, 0.0},
+                                                                   {0.0, 1.0, 0.0, 0.0, 0.0},
+                                                                   {0.0, 0.0, 1.0, 0.0, 0.0},
+                                                                   {0.0, 0.0, 0.0, 1.0, 0.0},
+                                                                   {0.0, 0.0, 0.0, 0.0, 1.0},
+                                                               },
+                                                           .factor = 1.0 / 5.0,
+                                                           .bias = 0.0};
+const medium_convolution_filter medium_motion_blur_filter = {
+    .matrix =
+        {
+            {1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+            {0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+            {0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0},
+            {0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0},
+            {0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0},
+            {0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0},
+            {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0},
+
+        },
+    .factor = 1.0 / 7.0,
     .bias = 0.0};
+const big_convolution_filter big_motion_blur_filter = {
+    .matrix =
+        {
+            {1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+            {0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+            {0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+            {0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+            {0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0},
+            {0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0},
+            {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0},
+            {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0},
+            {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0},
+        },
+    .factor = 1.0 / 9.0,
+    .bias = 0.0};
+
+convolution_filter *create_motion_blur_convolution_filter(enum strength str)
+{
+    switch (str)
+    {
+    case SMALL:
+        convolution_filter *filter = malloc(sizeof(convolution_filter));
+        if (!filter)
+        {
+            error("ERROR: malloc failed\n");
+            return NULL;
+        }
+        filter->matrix = small_motion_blur_filter.matrix;
+        filter->bias = small_motion_blur_filter.bias;
+        filter->factor = small_motion_blur_filter.factor;
+        filter->size = SMALL_FILTER_SIZE;
+        return filter;
+    case MEDIUM:
+        convolution_filter *filter = malloc(sizeof(convolution_filter));
+        if (!filter)
+        {
+            error("ERROR: malloc failed\n");
+            return NULL;
+        }
+        filter->matrix = medium_motion_blur_filter.matrix;
+        filter->bias = medium_motion_blur_filter.bias;
+        filter->factor = medium_motion_blur_filter.factor;
+        filter->size = MEDIUM_FILTER_SIZE;
+        return filter;
+    case BIG:
+        convolution_filter *filter = malloc(sizeof(convolution_filter));
+        if (!filter)
+        {
+            error("ERROR: malloc failed\n");
+            return NULL;
+        }
+        filter->matrix = big_motion_blur_filter.matrix;
+        filter->bias = big_motion_blur_filter.bias;
+        filter->factor = big_motion_blur_filter.factor;
+        filter->size = BIG_FILTER_SIZE;
+        return filter;
+    default:
+        error("ERROR: invalid filter size");
+        return NULL;
+    }
+}
+
+//----------------------------------------------------------------------------------------------
+// Find edges
+//----------------------------------------------------------------------------------------------
+
+const small_convolution_filter small_find_edges_filter = {.matrix =
+                                                              {
+                                                                  {-1.0, -1.0, -1.0, -1.0, -1.0},
+                                                                  {-1.0, -1.0, -1.0, -1.0, -1.0},
+                                                                  {-1.0, -1.0, 24.0, -1.0, -1.0},
+                                                                  {-1.0, -1.0, -1.0, -1.0, -1.0},
+                                                                  {-1.0, -1.0, -1.0, -1.0, -1.0},
+                                                              },
+                                                          .factor = 1.0,
+                                                          .bias = 0.0};
+const medium_convolution_filter medium_find_edges_filter = {
+    .matrix =
+        {
+            {-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0},
+            {-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0},
+            {-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0},
+            {-1.0, -1.0, -1.0, 48.0, -1.0, -1.0, -1.0},
+            {-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0},
+            {-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0},
+            {-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0},
+
+        },
+    .factor = 1.0,
+    .bias = 0.0};
+const big_convolution_filter big_find_edges_filter = {
+    .matrix =
+        {
+            {-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0},
+            {-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0},
+            {-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0},
+            {-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0},
+            {-1.0, -1.0, -1.0, -1.0, 80.0, -1.0, -1.0, -1.0, -1.0},
+            {-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0},
+            {-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0},
+            {-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0},
+            {-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0},
+        },
+    .factor = 1.0,
+    .bias = 0.0};
+
+convolution_filter *create_find_edges_convolution_filter(enum strength str)
+{
+    switch (str)
+    {
+    case SMALL:
+        convolution_filter *filter = malloc(sizeof(convolution_filter));
+        if (!filter)
+        {
+            error("ERROR: malloc failed\n");
+            return NULL;
+        }
+        filter->matrix = small_find_edges_filter.matrix;
+        filter->bias = small_find_edges_filter.bias;
+        filter->factor = small_find_edges_filter.factor;
+        filter->size = SMALL_FILTER_SIZE;
+        return filter;
+    case MEDIUM:
+        convolution_filter *filter = malloc(sizeof(convolution_filter));
+        if (!filter)
+        {
+            error("ERROR: malloc failed\n");
+            return NULL;
+        }
+        filter->matrix = medium_find_edges_filter.matrix;
+        filter->bias = medium_find_edges_filter.bias;
+        filter->factor = medium_find_edges_filter.factor;
+        filter->size = MEDIUM_FILTER_SIZE;
+        return filter;
+    case BIG:
+        convolution_filter *filter = malloc(sizeof(convolution_filter));
+        if (!filter)
+        {
+            error("ERROR: malloc failed\n");
+            return NULL;
+        }
+        filter->matrix = big_find_edges_filter.matrix;
+        filter->bias = big_find_edges_filter.bias;
+        filter->factor = big_find_edges_filter.factor;
+        filter->size = BIG_FILTER_SIZE;
+        return filter;
+    default:
+        error("ERROR: invalid filter size");
+        return NULL;
+    }
+}
+
+//----------------------------------------------------------------------------------------------
+// Emboss
+//----------------------------------------------------------------------------------------------
+
+const small_convolution_filter small_emboss_filter = {.matrix =
+                                                          {
+                                                              {-1.0, -1.0, -1.0, -1.0, 0.0},
+                                                              {-1.0, -1.0, -1.0, 0.0, 1.0},
+                                                              {-1.0, -1.0, 0.0, 1.0, 1.0},
+                                                              {-1.0, 0.0, 1.0, 1.0, 1.0},
+                                                              {0.0, 1.0, 1.0, 1.0, 1.0},
+                                                          },
+                                                      .factor = 1.0,
+                                                      .bias = 128.0};
+const medium_convolution_filter medium_emboss_filter = {
+    .matrix =
+        {
+            {-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 0.0},
+            {-1.0, -1.0, -1.0, -1.0, -1.0, 0.0, 1.0},
+            {-1.0, -1.0, -1.0, -1.0, 0.0, 1.0, 1.0},
+            {-1.0, -1.0, -1.0, 0.0, 1.0, 1.0, 1.0},
+            {-1.0, -1.0, 0.0, 1.0, 1.0, 1.0, 1.0},
+            {-1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0},
+            {0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0},
+
+        },
+    .factor = 1.0,
+    .bias = 128.0};
+const big_convolution_filter big_emboss_filter = {
+    .matrix =
+        {
+            {-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 0.0},
+            {-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 0.0, 1.0},
+            {-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 0.0, 1.0, 1.0},
+            {-1.0, -1.0, -1.0, -1.0, -1.0, 0.0, 1.0, 1.0, 1.0},
+            {-1.0, -1.0, -1.0, -1.0, 0.0, 1.0, 1.0, 1.0, 1.0},
+            {-1.0, -1.0, -1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0},
+            {-1.0, -1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0},
+            {-1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0},
+            {0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0},
+        },
+    .factor = 1.0,
+    .bias = 128.0};
+
+convolution_filter *create_emboss_convolution_filter(enum strength str)
+{
+    switch (str)
+    {
+    case SMALL:
+        convolution_filter *filter = malloc(sizeof(convolution_filter));
+        if (!filter)
+        {
+            error("ERROR: malloc failed\n");
+            return NULL;
+        }
+        filter->matrix = small_emboss_filter.matrix;
+        filter->bias = small_emboss_filter.bias;
+        filter->factor = small_emboss_filter.factor;
+        filter->size = SMALL_FILTER_SIZE;
+        return filter;
+    case MEDIUM:
+        convolution_filter *filter = malloc(sizeof(convolution_filter));
+        if (!filter)
+        {
+            error("ERROR: malloc failed\n");
+            return NULL;
+        }
+        filter->matrix = medium_emboss_filter.matrix;
+        filter->bias = medium_emboss_filter.bias;
+        filter->factor = medium_emboss_filter.factor;
+        filter->size = MEDIUM_FILTER_SIZE;
+        return filter;
+    case BIG:
+        convolution_filter *filter = malloc(sizeof(convolution_filter));
+        if (!filter)
+        {
+            error("ERROR: malloc failed\n");
+            return NULL;
+        }
+        filter->matrix = big_emboss_filter.matrix;
+        filter->bias = big_emboss_filter.bias;
+        filter->factor = big_emboss_filter.factor;
+        filter->size = BIG_FILTER_SIZE;
+        return filter;
+    default:
+        error("ERROR: invalid filter size");
+        return NULL;
+    }
+}
--- src/filters/filters.h
+++ src/filters/filters.h
@@ -1,60 +1,52 @@
 #ifndef FILTERS_H
 #define FILTERS_H
 
-#define FLYWEIGHT_FILTER_WIDTH 3
-#define FLYWEIGHT_FILTER_HEIGHT 3
+enum effect {
+    BLUR = 1,
+    MOTION_BLUR = 2,
+    FIND_EDGES = 3,
+    EMBOSS = 4,
+};
 
-typedef struct
+enum strength
 {
-    double matrix[FLYWEIGHT_FILTER_HEIGHT][FLYWEIGHT_FILTER_WIDTH];
-    double factor;
-    double bias;
-} tiny_convolution_filter;
+    SMALL = 1,
+    MEDIUM = 2,
+    BIG = 3,
+};
 
-extern tiny_convolution_filter tiny_blur_filter;
-
-#define SMALL_FILTER_WIDTH 5
-#define SMALL_FILTER_HEIGHT 5
-    typedef struct
-{
-    double matrix[FLYWEIGHT_FILTER_HEIGHT][FLYWEIGHT_FILTER_WIDTH];
+#define SMALL_FILTER_SIZE 5
+typedef struct {
+    double matrix[SMALL_FILTER_SIZE][SMALL_FILTER_SIZE];
     double factor;
     double bias;
 } small_convolution_filter;
 
 extern small_convolution_filter small_blur_filter;
 
-#define MEDIUM_FILTER_WIDTH 7
-#define MEDIUM_FILTER_HEIGHT 7
-    typedef struct
-{
-    double matrix[FLYWEIGHT_FILTER_HEIGHT][FLYWEIGHT_FILTER_WIDTH];
+#define MEDIUM_FILTER_SIZE 7
+typedef struct {
+    double matrix[MEDIUM_FILTER_SIZE][MEDIUM_FILTER_SIZE];
     double factor;
     double bias;
 } medium_convolution_filter;
 
 extern medium_convolution_filter medium_blur_filter;
 
-#define BIG_FILTER_WIDTH 9
-#define BIG_FILTER_HEIGHT 9
-    typedef struct
-{
-    double matrix[FLYWEIGHT_FILTER_HEIGHT][FLYWEIGHT_FILTER_WIDTH];
+#define BIG_FILTER_SIZE 9
+typedef struct {
+    double matrix[BIG_FILTER_SIZE][BIG_FILTER_SIZE];
     double factor;
     double bias;
 } big_convolution_filter;
 
 extern big_convolution_filter big_blur_filter;
 
-#define HUGE_FILTER_WIDTH 15
-#define HUGE_FILTER_HEIGHT 15
-    typedef struct
-{
-    double matrix[FLYWEIGHT_FILTER_HEIGHT][FLYWEIGHT_FILTER_WIDTH];
+typedef struct {
+    double **matrix;
     double factor;
     double bias;
-} huge_convolution_filter;
-
-extern huge_convolution_filter huge_blur_filter;
+    int size;
+} convolution_filter;
 
 #endif
